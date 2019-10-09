d604dda2483461ca4b182abf5e5dd04952ab9866
chris <chris@seasoft.com.au>
2019-10-10 06:16:02 +0800

    Working as expected here.

--- src/app/client_app.cljs
+++
@@ -1,5 +0,0 @@
-(ns app.client-app
-  (:require [com.fulcrologic.fulcro.networking.http-remote :as http]
-            [com.fulcrologic.fulcro.application :as app]))
-
-(defonce APP (app/fulcro-app {:remotes {:remote (http/fulcro-http-remote {})}}))
--- src/app/ui.cljs
+++ src/app/ui.cljs
@@ -1,13 +1,13 @@
 (ns app.ui
   (:require [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-            [com.fulcrologic.fulcro.dom :as dom]))
+            [com.fulcrologic.fulcro.dom :as dom :refer [span div button br]]))
 
 (defsc Product [this
                 {:product/keys [description price]}
                 {:keys [quantity]}]
   {:query [:product/id :product/description :product/price]
    :ident :product/id}
-  (dom/span (str description ", quantity: " quantity ", price: $" price ", extension: $" (* price quantity))))
+  (span (str description ", quantity: " quantity ", price: $" price ", extension: $" (* price quantity))))
 (def product-ui (comp/factory Product))
 
 (defsc Organisation [this {:organisation/keys [id products]}]
@@ -17,7 +17,7 @@
 (defsc LineItem [this {:line-item/keys [id product quantity]}]
   {:query         [:line-item/id {:line-item/product (comp/get-query Product)} :line-item/quantity]
    :ident         :line-item/id}
-  (dom/div (product-ui (comp/computed product {:quantity quantity}))))
+  (div (product-ui (comp/computed product {:quantity quantity}))))
 
 (def line-item-ui (comp/factory LineItem))
 
@@ -28,12 +28,12 @@
    :initial-state (fn [_] {:invoice/id 1})
    :ident         :invoice/id}
   (assert onClick)
-  (dom/div
-    (dom/button {:onClick #(onClick)}
+  (div
+    (button {:onClick #(onClick)}
                 "Load Invoice")
-    (dom/div (str "Num line items to show is " (count line-items)))
-    (dom/br)
+    (div (str "Num line items to show is " (count line-items)))
+    (br)
     ;; If you apply, react doesn't complain, you don't need a keyfn
-    (apply dom/div (map line-item-ui line-items))))
+    (apply div (map line-item-ui line-items))))
 
 (def invoice-ui (comp/factory Invoice))
